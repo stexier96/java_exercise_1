c5088a6d1754c3321989a03e4e3b0e90f04c7247
stexier96 <stexier@et.esiea.fr>
2021-11-29 15:36:24 +0100

    Exercice 4 restructuré

--- src/Launcher.java
+++ src/Launcher.java
@@ -9,28 +9,34 @@ public class Launcher {
         System.out.println("Bienvenue");
         String str = null;
         int i = 0;
-        Scanner entry = new Scanner(System.in);
-        str = entry.next();
-        while (str.equals("quit") == FALSE && str.equals("fibo") == FALSE) {
-            System.out.println(str + " Unknown command");
-            entry = new Scanner(System.in);
+        do {
+            System.out.println("Veuillez entrer une commande");
+            Scanner entry = new Scanner(System.in);
             str = entry.next();
 
-        }
+            if (str.equals("fibo") == TRUE) {
+                System.out.println("Commencer par quel nombre?");
+                entry = new Scanner(System.in);
+                i = entry.nextInt();
 
-        if (str.equals("fibo") == TRUE) {
-            System.out.println("Commencer par quel nombre?");
-            entry = new Scanner(System.in);
-            i = entry.nextInt();
-
-            int a=0,b = 1, fibo = 0;
-            for (int j = 1; j < i; j++){
-                fibo = a + b;
-                a = b;
-                b = fibo;
+                int a=0,b = 1, fibo = 0;
+                for (int j = 1; j < i; j++){
+                    fibo = a + b;
+                    a = b;
+                    b = fibo;
+                }
+                System.out.println("Fibo à l'indice " + i + " = " + fibo);
             }
-            System.out.println("Fibo à l'indice " + i + " = " + fibo);
-        }
+
+            else if (str.equals("quit") == TRUE){
+                System.out.println("Exiting program");
+            }
+
+            else {
+                System.out.println(str + " Unknown command, try again");
+            }
+
+        } while (str.equals("quit") == FALSE);
 
     }
 }
