9a6ef937f22c41325d50330b0c18d9da905753fe
stexier96 <stexier@et.esiea.fr>
2021-11-29 15:28:51 +0100

    Exercice 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 
 public class Launcher {
 
@@ -7,12 +8,29 @@ public class Launcher {
 
         System.out.println("Bienvenue");
         String str = null;
+        int i = 0;
         Scanner entry = new Scanner(System.in);
         str = entry.next();
-        while (str.equals("quit") == FALSE) {
+        while (str.equals("quit") == FALSE && str.equals("fibo") == FALSE) {
             System.out.println(str + " Unknown command");
             entry = new Scanner(System.in);
             str = entry.next();
+
         }
+
+        if (str.equals("fibo") == TRUE) {
+            System.out.println("Commencer par quel nombre?");
+            entry = new Scanner(System.in);
+            i = entry.nextInt();
+
+            int a=0,b = 1, fibo = 0;
+            for (int j = 1; j < i; j++){
+                fibo = a + b;
+                a = b;
+                b = fibo;
+            }
+            System.out.println("Fibo à l'indice " + i + " = " + fibo);
+        }
+
     }
 }
