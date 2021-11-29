b66081158e9dd89f6df25c36470ae62fe5f2b361
stexier96 <stexier@et.esiea.fr>
2021-11-29 15:11:59 +0100

    Exercice 3

--- src/Launcher.java
+++ src/Launcher.java
@@ -6,10 +6,13 @@ public class Launcher {
     public static void main(String[] args) {
 
         System.out.println("Bienvenue");
+        String str = null;
         Scanner entry = new Scanner(System.in);
-        String str = entry.next();
-        if (str.equals("quit") == FALSE){
+        str = entry.next();
+        while (str.equals("quit") == FALSE) {
             System.out.println(str + " Unknown command");
+            entry = new Scanner(System.in);
+            str = entry.next();
         }
     }
 }
