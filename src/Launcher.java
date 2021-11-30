68c9c73ec95fe753ad46947d2f49fb82bad83a00
stexier96 <stexier@et.esiea.fr>
2021-11-30 09:37:17 +0100

    Exercice 5

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,10 +1,17 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 
 public class Launcher {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         System.out.println("Bienvenue");
         String str = null;
@@ -14,7 +21,41 @@ public class Launcher {
             Scanner entry = new Scanner(System.in);
             str = entry.next();
 
-            if (str.equals("fibo") == TRUE) {
+            if (str.equals("freq") == TRUE) {
+                System.out.println("Indiquez le chemin du fichier a analyser");
+                entry = new Scanner(System.in);
+                String path = entry.next();
+                try {
+                    java.nio.file.Files.readString(Paths.get(path));
+                }
+                catch(Exception e) {
+                    System.out.println(e.getClass().getSimpleName());
+                    break;
+                }
+                System.out.println("Analyse du fichier " + path);
+                String texte = java.nio.file.Files.readString(Paths.get(path))
+                        .replaceAll("(,)", "")
+                        .replaceAll("\\.", "")
+                        .toLowerCase();
+
+                String[] texteTab = texte.split(" ");
+
+                Map<String,Long> mots=  Arrays
+                        .stream(texteTab)
+                        .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+
+                Map<String ,Long> freq3 =
+                        mots.entrySet().stream()
+                                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                                .limit(3)
+                                .collect(Collectors.toMap(
+                                        Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+                System.out.println(freq3);
+                //sortedList.forEach(System.out::println);
+            }
+
+            else if (str.equals("fibo") == TRUE) {
                 System.out.println("Commencer par quel nombre?");
                 entry = new Scanner(System.in);
                 i = entry.nextInt();
@@ -25,7 +66,7 @@ public class Launcher {
                     a = b;
                     b = fibo;
                 }
-                System.out.println("Fibo à l'indice " + i + " = " + fibo);
+                System.out.println("Fibo a l'indice " + i + " = " + fibo);
             }
 
             else if (str.equals("quit") == TRUE){
